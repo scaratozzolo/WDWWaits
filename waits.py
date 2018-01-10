48729e844b1707b8267460f6c39c2256959f8c13
Scott Caratozzolo <scaratozzolo12@gmail.com>
2018-01-10 18:29:39 -0500

    bugs

--- waits.py
+++ waits.py
@@ -10,50 +10,67 @@ import requests
 import time
 from datetime import datetime
 import pickle
+import json
 import sys
 import os
 
-if os.path.exists('ridedata'):
-    ride_data = pickle.load(open('ridedata', 'rb'))
+if os.path.exists('ridedata.p'):
+    ride_data = pickle.load(open('ridedata.p', 'rb'))
 else:
     ride_data = {}
 
+
 sys.setrecursionlimit(5000)
 
-loops = 5
-counter = 0
 
-while loops != counter:
-    
-    locations = []
-    times = []
-    
-    html = requests.get('https://www.easywdw.com/waits/?&park=All&sort=time&showOther=false').content
-    soup = BeautifulSoup(html, 'lxml')
-    
-    all_h2 = soup.find_all('h2')
-    
-    for i, elm in enumerate(all_h2[:-2]):
-        if elm.string == 'Ride' or elm.string == 'Location' or elm.string == 'Time':
-            continue
-        elif (i+1) % 3 == 1:
-            if elm.string in ride_data:
-                continue
-            else:
-                ride_data[elm.string] = {'Times' : []}
-        elif (i+1) % 3 == 2:
-            locations.append(elm.string)
-        elif (i+1) % 3 == 0:
-            times.append(elm.string)
-    
-         
-    for i, ride in enumerate(ride_data):
-        ride_data[ride]['Times'].append([datetime.now(), times[i]])
-        ride_data[ride]['Location'] = locations[i]
+
+def get_data():
+
+
+    for _ in range(5):
+        rides = []
+        locations = []
+        times = []
         
-    print(ride_data)
-    counter += 1
-    pickle.dump(ride_data, open('ridedata' , 'wb'))
-    time.sleep(900)
+        html = requests.get('https://www.easywdw.com/waits/?&park=All&sort=time&showOther=false').content
+        soup = BeautifulSoup(html, 'lxml')
+        
+        all_h2 = soup.find_all('h2')
+        
+        for i, elm in enumerate(all_h2[:-2]):
+            if elm.string == 'Ride' or elm.string == 'Location' or elm.string == 'Time':
+                continue
+            elif (i+1) % 3 == 1:
+                rides.append(elm.string)
+                if elm.string in ride_data:
+                    continue
+                else:
+                    ride_data[elm.string] = {'Times' : {}}
+            elif (i+1) % 3 == 2:
+                locations.append(elm.string)
+            elif (i+1) % 3 == 0:
+                times.append(int(elm.string.strip()[:-4]))
+        
+            
+        for i, ride in enumerate(rides):
+            ride_data[ride]['Times'][str(datetime.now())] = times[i]
+            ride_data[ride]['Location'] = locations[i]
+        
+        print(datetime.now())
+        print(ride_data)
+
+        with open('ridedata.json', 'w') as f:
+            json.dump(ride_data, f)
+        pickle.dump(ride_data, open('ridedata.p', 'wb'))
+        time.sleep(900)
 
 
+if __name__ == '__main__':
+#     while True:
+#         if datetime.now().minute % 15 == 0:
+#             get_data()
+#             break
+#         
+#         time.sleep(60)
+
+    get_data()
