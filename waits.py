9149b5f91b7f6e4b19d8f351b602727f6b1fb56a
Scott Caratozzolo <scaratozzolo12@gmail.com>
2018-01-17 22:55:27 -0500

    Bug fixes, new console output, running during real park hours

--- waits.py
+++ waits.py
@@ -4,13 +4,13 @@ Created on Jan 10, 2018
 @author: Scott Caratozzolo
 
 '''
-from bs4 import BeautifulSoup
 import requests
 import time
-from datetime import datetime
-# import pickle
+from datetime import datetime, timedelta
 import json
 import os
+from tqdm import tqdm
+
 
 
 
@@ -27,7 +27,7 @@ if os.path.exists('checkpoints'):                   #checks for ride_data.json a
     else:
         ride_data = {}                              #if it doesn't exist, ride_data is an empty dictionary
 
-park_id = {'Magic Kingdom':'80007944','Epcot':'80007838','Animal Kingdom':'80007823','Hollywood Studios':'80007998'}
+park_id = {'Magic Kingdom Park':'80007944','Epcot':'80007838',"Disney's Animal Kingdom Theme Park":'80007823',"Disney's Hollywood Studios":'80007998'}
 
 
 PAUSE_TIME = 15                                     #pause time between wait time gathering in minutes
@@ -42,6 +42,8 @@ def get_data():
 #               
 #         time.sleep(2)
 
+    parkopen, parkclose = park_hours()
+
     counter = 1
     while True:                                     #main program
         
@@ -53,31 +55,34 @@ def get_data():
         locations = []
         times = []
         parks = []
-            
-        YEAR = datetime.now().year
-        MONTH = datetime.now().month
-        DAY = datetime.now().day
+        
+        YEAR = datetime.today().year
+        MONTH = datetime.today().month
+        DAY = datetime.today().day
             
         
         for park in park_id:
             s = requests.get("https://api.wdpro.disney.go.com/facility-service/theme-parks/{}/wait-times".format(park_id[park]), headers=headers)
         
             data = json.loads(s.content)
-            
-            for i in range(len(data['entries'])):
+            print('Getting wait times for {}'.format(park))
+            for i in tqdm(range(len(data['entries']))):
                 if 'postedWaitMinutes' in data['entries'][i]['waitTime']:
                     rides.append(data['entries'][i]['name'])
                     times.append(data['entries'][i]['waitTime']['postedWaitMinutes'])
-                    parks.append(park)
                     if data['entries'][i]['type'] == 'Attraction':
                         q = requests.get("https://api.wdpro.disney.go.com/facility-service/attractions/{}".format(data['entries'][i]['id']), headers=headers)
                         ride_info = json.loads(q.content)
                         locations.append(ride_info['links']['ancestorLand']['title'])
+                        parks.append(ride_info['links']['ancestorThemePark']['title'])
                     else:
-                        locations.append(park)
+                        locations.append('{} Entertainment'.format(park))
+                        parks.append(park)
+                        
 
-#         print(len(rides), len(locations), len(times), len(parks))
+#         print(len(rides), len(locations), len(times), len(parks), len(status))
         if len(rides) != 0:   
+            print('Adding new data...')
             for i, ride in enumerate(rides):            #adds new times and location to ride dictionary...location is added in case its a new location or previously was none
                 if ride in ride_data:
                     ride_data[ride]['Times'][str(datetime.now())] = times[i]
@@ -89,7 +94,7 @@ def get_data():
                     ride_data[ride]['Location'] = locations[i].replace(u"\u2013", "-")
                     ride_data[ride]['Park'] = parks[i]
         
-        print('{}. New data added at {}'.format(counter, datetime.now()))
+        print('{}. Finsihed at {}'.format(counter, datetime.now()))
         counter += 1
 #         print(ride_data)
 
@@ -97,7 +102,10 @@ def get_data():
             json.dump(ride_data, f)
         
         
-        if datetime.now().hour < 6:                #if time is midnight, waits until 6 am to start again
+        
+        
+        
+        if datetime.now().hour < parkopen or datetime.now().hour >= parkclose:                #if time is midnight, waits until 6 am to start again
             location_data = {}
  
             for key in ride_data:
@@ -118,18 +126,87 @@ def get_data():
             with open('checkpoints/bylocation/ridedata-location-{}-{}-{}.json'.format(YEAR, MONTH, DAY), 'w') as f:       #writes ride_data to json file
                 json.dump(location_data, f)
             
-            print('All parks closed at {}:{} '.format(datetime.now().hour, datetime.now().minute))
-            time.sleep((6-datetime.now().hour)*3600)
-            print('Parks opening soon: {}:{}'.format(datetime.now().hour, datetime.now().minute))
+            tempparkopen = parkopen
+            tempparkclose = parkclose
+            
+            MINUTE = datetime.now().minute
+            
+            if len(str(MINUTE)) < 2:
+                MINUTE = '0'+str(MINUTE)
+            
+            parkopen, parkclose = park_hours()
+            
+            print('All parks are closed at {}:{}. They will reopen at {}:00'.format(datetime.now().hour, MINUTE, tempparkopen))
+            time.sleep((tempparkopen-(tempparkclose - 24))*3600)
+            print('Parks opening at: {}:00'.format(tempparkopen))
         else:
             time.sleep(PAUSE_TIME*60)                             #waits 10 minutes before starting again
 
 
+def park_hours():
+    
+    print('Getting park hours...')
+    r = requests.get("https://disneyworld.disney.go.com/authentication/get-client-token", headers={'User-Agent': 'Chrome/63.0.3239.132'})
+    data = json.loads(r.text)
+    headers = {"Authorization":"BEARER {}".format(data['access_token']),'User-Agent': 'Chrome/63.0.3239.132'}
+    
+    YEAR = str(datetime.today().year)
+    MONTH = str(datetime.today().month)
+    DAY = str(datetime.today().day)
+    
+    
+    if len(MONTH) < 2:
+        MONTH = '0'+MONTH
+        
+    if len(DAY) < 2:
+        DAY = '0'+DAY
+        
+    tomorrow = datetime.today() + timedelta(days=1)
+    
+    TOMORROWYEAR = str(tomorrow.year)
+    TOMORROWMONTH = str(tomorrow.month)
+    TOMORROWDAY = str(tomorrow.day)
+    
+    if len(TOMORROWMONTH) < 2:
+        TOMORROWMONTH = '0'+TOMORROWMONTH
+        
+    if len(TOMORROWDAY) < 2:
+        TOMORROWDAY = '0'+TOMORROWDAY
+    
+    parkopen = 9
+    parkclose = 20
+    
+    
+    for park in tqdm(park_id):
+        # for park in park_id:
+        s = requests.get("https://api.wdpro.disney.go.com/global-pool-override-B/facility-service/schedules/{}".format(park_id[park]), headers=headers)
+        data = json.loads(s.content)
+        
+        
+        for i in range(len(data['schedules'])):
+            if data['schedules'][i]['date'] == '{}-{}-{}'.format(TOMORROWYEAR, TOMORROWMONTH, TOMORROWDAY):
+                if int(data['schedules'][i]['startTime'][:2]) < parkopen:
+                    parkopen = int(data['schedules'][i]['startTime'][:2])
+                    
+            if data['schedules'][i]['date'] == '{}-{}-{}'.format(YEAR, MONTH, DAY):        
+                if int(data['schedules'][i]['endTime'][:2]) > parkclose:
+                    parkclose = int(data['schedules'][i]['endTime'][:2])
+    
+    return parkopen, parkclose
+
+
 if __name__ == '__main__':
-    try:
-        print('Starting')
-        get_data()
-    except KeyboardInterrupt:
-        print('Keyboard Interruption, program ended')
+    while True:
+        try:
+            print('Starting')
+            get_data()
+        except KeyboardInterrupt:
+            print('Keyboard Interruption, program ended')
+            break
+        except ConnectionError:
+            print("Couldn't get new data")
+            time.sleep(PAUSE_TIME*60) 
+
+        
         
         
