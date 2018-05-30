e945e6a45adecd82a8644e9e7fd9cf7d948db5b4
Scott Caratozzolo <scaratozzolo12@gmail.com>
2018-05-30 17:20:51 -0400

    Bug fixes, added weather, ridedata files are day specific not cumulative

--- waits.py
+++ waits.py
@@ -15,24 +15,11 @@ from tqdm import tqdm
 
 destinations = {"Walt Disney World Resort" : "80007798", "Disneyland Resort" : "80008297"}
 
-if not os.path.exists('checkpoints'):               #checks for checkpoints directory and creates it
-    os.makedirs('checkpoints')
-    os.makedirs('checkpoints/ridedata')
-    os.makedirs('checkpoints/bylocation')
-
-if os.path.exists('checkpoints'):                   #checks for ride_data.json and loads it to ride_data as a dictionary if it exists
-    file_list = os.listdir('checkpoints/ridedata')
-    if len(file_list) != 0:
-        with open('checkpoints/ridedata/{}'.format(file_list[len(file_list)-1]), 'r') as f:
-            ride_data = json.load(f)
-    else:
-        ride_data = {}
-
 PAUSE_TIME = 15                                     #pause time between wait time gathering in minutes
 
 wdw = Destination("80007798")
 dl = Destination("80008297")
-parks_dict = {}
+parks_dict = {"WDW": wdw.getThemeParks() + wdw.getWaterParks(), "DL": dl.getThemeParks()}
 
 def get_data():
 
@@ -43,6 +30,10 @@ def get_data():
 
         TODAY = datetime.today()
         TOMORROW = datetime.today() + timedelta(days=1)
+
+        global ride_data
+        if counter > 1:
+            ride_data = {}
 
         parkopen, parkclose = all_hours(TODAY.year, TODAY.month, TODAY.day)
 
@@ -60,60 +51,102 @@ def get_data():
                         attractions.append(attr)
 
                 rides = []
-                locations = []
+                ids = []
                 times = []
+                locations = []
                 parks = []
 
-                YEAR = datetime.today().year
-                MONTH = datetime.today().month
-                DAY = datetime.today().day
                 NOW = datetime.now()
 
                 print("Getting wait times...")
                 for attr in tqdm(attractions):
-                    rides.append(attr.getAttractionName())
-                    locations.append(attr.getAncestorLand())
-                    times.append(attr.getAttractionWaitTime())
-                    parks.append(attr.getAncestorThemePark().getParkName())
+                    park = attr.getAncestorThemePark()
+                    for theme in parks_dict["WDW"]:
+                        if theme == park:
+                            rides.append(attr.getAttractionName() + " - WDW")
+                            ids.append(attr.getAttractionID())
+                            times.append(attr.getAttractionWaitTime())
+                            locations.append(attr.getAncestorLand())
+                            parks.append(park.getParkName())
+
+                    for theme in parks_dict["DL"]:
+                        if theme == park:
+                            rides.append(attr.getAttractionName() + " - DL")
+                            ids.append(attr.getAttractionID())
+                            times.append(attr.getAttractionWaitTime())
+                            locations.append(attr.getAncestorLand())
+                            parks.append(park.getParkName())
 
                 if len(rides) != 0:
                     print('Adding new data...')
                     for i, ride in enumerate(rides):            #adds new times and location to ride dictionary...location is added in case its a new location or previously was none
                         if ride in ride_data:
                             ride_data[ride]['Times'][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))] = times[i]
+                            ride_data[ride]["ID"] = ids[i]
                             ride_data[ride]['Location'] = locations[i].replace(u"\u2013", "-")
                             ride_data[ride]['Park'] = parks[i]
                         else:
                             ride_data[ride] = {'Times' : {}}
+                            ride_data[ride]["ID"] = ids[i]
                             ride_data[ride]['Times'][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))] = times[i]
                             ride_data[ride]['Location'] = locations[i].replace(u"\u2013", "-")
                             ride_data[ride]['Park'] = parks[i]
+
+                    weather_data = requests.get("https://api.openweathermap.org/data/2.5/group?id=4167147,5323810&units=imperial&appid=07e91d416dbdfcbd7d7ab9c8096ac687").json()
+                    for entry in weather_data["list"]:
+                        if entry["name"] in ride_data:
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))] = {}
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["weather"] = entry["weather"]
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["main"] = entry["main"]
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["wind"] = entry["wind"]
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["clouds"] = entry["clouds"]
+                            try:
+                                ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["rain"] = entry["rain"]
+                            except:
+                                pass
+                        else:
+                            ride_data[entry["name"]] = {}
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))] = {}
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["weather"] = entry["weather"]
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["main"] = entry["main"]
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["wind"] = entry["wind"]
+                            ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["clouds"] = entry["clouds"]
+                            try:
+                                ride_data[entry["name"]][str(datetime(NOW.year, NOW.month, NOW.day, NOW.hour, NOW.minute))]["rain"] = entry["rain"]
+                            except:
+                                pass
+
                 print('{}. Finsihed at {}'.format(counter, datetime.now()))
                 counter += 1
 
-                with open('checkpoints/ridedata/ridedata-{}-{}-{}.json'.format(YEAR, formatDate(str(MONTH)), formatDate(str(DAY))), 'w') as f:       #writes ride_data to json file
+                with open('checkpoints/ridedata/ridedata-{}-{}-{}.json'.format(TODAY.year, formatDate(str(TODAY.month)), formatDate(str(TODAY.day))), 'w') as f:       #writes ride_data to json file
                     json.dump(ride_data, f)
-
 
                 if datetime.now() < parkopen or datetime.now() >= parkclose:
                     location_data = {}
 
                     for key in ride_data:
-                        if ride_data[key]["Park"] in location_data:
-                            if ride_data[key]['Location'] in location_data[ride_data[key]["Park"]]:
-                                location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key] = {}
-                                location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key]['Times'] = ride_data[key]['Times']
+                        if key != "Orlando" or key != "Anaheim":
+                            if ride_data[key]["Park"] in location_data:
+                                if ride_data[key]['Location'] in location_data[ride_data[key]["Park"]]:
+                                    location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key] = {}
+                                    location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key]['Times'] = ride_data[key]['Times']
+                                    location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key]['ID'] = ride_data[key]['ID']
+                                else:
+                                    location_data[ride_data[key]["Park"]][ride_data[key]["Location"]] = {}
+                                    location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key] = {}
+                                    location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key]['Times'] = ride_data[key]['Times']
+                                    location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key]['ID'] = ride_data[key]['ID']
                             else:
+                                location_data[ride_data[key]["Park"]] = {}
                                 location_data[ride_data[key]["Park"]][ride_data[key]["Location"]] = {}
                                 location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key] = {}
                                 location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key]['Times'] = ride_data[key]['Times']
+                                location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key]['ID'] = ride_data[key]['ID']
                         else:
-                            location_data[ride_data[key]["Park"]] = {}
-                            location_data[ride_data[key]["Park"]][ride_data[key]["Location"]] = {}
-                            location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key] = {}
-                            location_data[ride_data[key]["Park"]][ride_data[key]["Location"]][key]['Times'] = ride_data[key]['Times']
+                            location_data[key] = ride_data[key]
 
-                    with open('checkpoints/bylocation/ridedata-location-{}-{}-{}.json'.format(YEAR, MONTH, DAY), 'w') as f:       #writes ride_data to json file
+                    with open('checkpoints/bylocation/ridedata-location-{}-{}-{}.json'.format(TODAY.year, formatDate(str(TODAY.month)), formatDate(str(TODAY.day))), 'w') as f:       #writes ride_data to json file
                         json.dump(location_data, f)
 
                     NOW = datetime.now()
@@ -138,8 +171,8 @@ def get_data():
 def all_hours(year, month, day):
     print("Getting park hours...")
     DATE = datetime(year, month, day)
-    parkopen = datetime(DATE.year, DATE.month, DATE.day, 9)
-    parkclose = datetime(DATE.year, DATE.month, DATE.day, 19)
+    parkopen = datetime(DATE.year, DATE.month, DATE.day, 12)
+    parkclose = datetime(DATE.year, DATE.month, DATE.day, 12)
 
     parks = parks_dict["WDW"] + parks_dict["DL"]
 
@@ -181,8 +214,8 @@ def park_hours(park, year, month, day):
 
     for item in parks_dict["WDW"]:
         if park == item:
-            parkopen = datetime(DATE.year, DATE.month, DATE.day, 9)
-            parkclose = datetime(DATE.year, DATE.month, DATE.day, 19)
+            parkopen = datetime(DATE.year, DATE.month, DATE.day, 12)
+            parkclose = datetime(DATE.year, DATE.month, DATE.day, 12)
 
             hours = park.getParkHours(year, month, day)
             if hours[2] != None:
@@ -199,8 +232,8 @@ def park_hours(park, year, month, day):
 
     for item in parks_dict["DL"]:
         if park == item:
-            parkopen = datetime(DATE.year, DATE.month, DATE.day, 9) + timedelta(hours=3)
-            parkclose = datetime(DATE.year, DATE.month, DATE.day, 19) + timedelta(hours=3)
+            parkopen = datetime(DATE.year, DATE.month, DATE.day, 15)
+            parkclose = datetime(DATE.year, DATE.month, DATE.day, 15)
             hours = park.getParkHours(year, month, day)
             if hours[2] != None:
                 if hours[2] + timedelta(hours=3) < parkopen:
@@ -217,9 +250,6 @@ def park_hours(park, year, month, day):
     return parkopen, parkclose
 
 def load_all_attractions():
-    """
-    in API get all attractions for a park, store all attractions per park in variables, depending on time of day (specific park hours) change the list
-    """
     print("Loading attractions...")
     raw_attractions = {}
     wdw = Destination("80007798")
@@ -239,14 +269,22 @@ def load_all_attractions():
 def load_attractions(raw_attractions):
 
     DATE = datetime.today()
+    # DATE = datetime(2018,5,28,7)
     attractions = []
 
     open = 0
     for theme in tqdm(raw_attractions):
         try:
             park = Park(theme)
-            parkopen, parkclose = park_hours(park, DATE.year, DATE.month, DATE.day)
-            if datetime.now() > parkopen and datetime.now() <= parkclose:
+            if DATE.hour >= 7:
+                parkopen, parkclose = park_hours(park, DATE.year, DATE.month, DATE.day)
+                # print(park, parkopen, parkclose, "1")
+            else:
+                YESTERDAY = DATE - timedelta(days=1)
+                parkopen, parkclose = park_hours(park, YESTERDAY.year, YESTERDAY.month, YESTERDAY.day)
+                # print(park, parkopen, parkclose, "2")
+
+            if datetime.now() >= parkopen and datetime.now() <= parkclose:
                 attractions += raw_attractions[theme]
                 open+=1
         except:
@@ -264,10 +302,26 @@ def formatDate(num):
     return num
 
 if __name__ == "__main__":
-    """
-    California is 3 hours behind us...do something about it.
-    """
+    # print(park_hours(Park("80007981"), 2018, 5, 26))
+    # print(park_hours(Park("336894"), 2018, 5, 26))
+    # load_attractions(load_all_attractions())
+    # print(park_hours(Park("336894"), 2018, 5, 26))
+    # print(park_hours(Park("330339"), 2018, 5, 26))
     print('Starting')
+
+    if not os.path.exists('checkpoints'):               #checks for checkpoints directory and creates it
+        os.makedirs('checkpoints')
+        os.makedirs('checkpoints/ridedata')
+        os.makedirs('checkpoints/bylocation')
+
+    TODAY = datetime.today()
+    if os.path.exists('checkpoints'):                   #checks for ride_data.json and loads it to ride_data as a dictionary if it exists
+        if os.path.isfile('checkpoints/ridedata/ridedata-{}-{}-{}.json'.format(TODAY.year, formatDate(str(TODAY.month)), formatDate(str(TODAY.day)))):
+            with open('checkpoints/ridedata/ridedata-{}-{}-{}.json'.format(TODAY.year, formatDate(str(TODAY.month)), formatDate(str(TODAY.day))), 'r') as f:
+                ride_data = json.load(f)
+        else:
+            ride_data = {}
+
     while True:
         try:
             get_data()
